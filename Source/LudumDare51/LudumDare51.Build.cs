1e2bfd7001c3476eed0d305952d44bfa710cc9f4
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-01 10:00:57 +0300

    Add module

--- Source/LudumDare51/LudumDare51.Build.cs
+++ Source/LudumDare51/LudumDare51.Build.cs
@@ -8,7 +8,7 @@ public class LudumDare51 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "TrickyPrototyping" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
