8fb8c878b805f62a3ec43861a9ac3d8c28f57002
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-02 15:36:23 +0300

    Implement stun component

---
+++ Source/LudumDare51/Private/Components/StunComponent.cpp
@@ -0,0 +1,44 @@
+﻿// "Copyright (c) 2022. Made by Title Goose Team during LudumDare 51. All rights reserved. "
+
+
+#include "Components/StunComponent.h"
+
+#include "GameFramework/Character.h"
+
+
+UStunComponent::UStunComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UStunComponent::ApplyStun(float Duration)
+{
+	if (IsStunned() || Duration <= 0.f) return;
+
+	// Character->DisableInput(Cast<APlayerController>(Character->GetController()));
+	GetWorld()->GetTimerManager().SetTimer(StunTimerHandle, this, &UStunComponent::EndStun, Duration, false);
+	OnStunApplied.Broadcast();
+}
+
+void UStunComponent::EndStun() const
+{
+	// Character->EnableInput(Cast<APlayerController>(Character->GetController()));
+	OnStunEnded.Broadcast();
+}
+
+
+bool UStunComponent::IsStunned() const
+{
+	return GetWorld()->GetTimerManager().IsTimerActive(StunTimerHandle);
+}
+
+void UStunComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UStunComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
---
+++ Source/LudumDare51/Public/Components/StunComponent.h
@@ -0,0 +1,48 @@
+﻿// "Copyright (c) 2022. Made by Title Goose Team during LudumDare 51. All rights reserved. "
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "StunComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnStunAppliedSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnStunEndedSignature);
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE51_API UStunComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UStunComponent();
+
+	UFUNCTION(BlueprintCallable, Category=Stun)
+	void ApplyStun(float Duration);
+
+	UFUNCTION(BlueprintCallable, Category=Stun)
+	bool IsStunned() const;
+
+	UPROPERTY(BlueprintAssignable, Category=Stun)
+	FOnStunAppliedSignature OnStunApplied;
+
+	UPROPERTY(BlueprintAssignable, Category=Stun)
+	FOnStunEndedSignature OnStunEnded;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadOnly)
+	ACharacter* Character = nullptr;
+
+	FTimerHandle StunTimerHandle;
+
+	UFUNCTION()
+	void EndStun() const;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
