b9c7a2b7e2dabb1a4d1fa41aeb32ec5b2f5e14dc
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-02 10:31:25 +0300

    Add sticker note for narrative

---
+++ Source/LudumDare51/Private/GameplayObjects/StickerNote.cpp
@@ -0,0 +1,41 @@
+﻿// "Copyright (c) 2022. Made by Title Goose Team during LudumDare 51. All rights reserved. "
+
+
+#include "GameplayObjects/StickerNote.h"
+
+#include "Components/TextRenderComponent.h"
+
+
+AStickerNote::AStickerNote()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	NoteMesh = CreateDefaultSubobject<UStaticMeshComponent>("NoteMesh");
+	SetRootComponent(NoteMesh);
+
+	NoteTextRenderer = CreateDefaultSubobject<UTextRenderComponent>("NoteTextRenderer");
+	NoteTextRenderer->SetupAttachment(GetRootComponent());
+}
+
+void AStickerNote::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	NoteTextRenderer->SetText(NoteText);
+}
+
+void AStickerNote::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AStickerNote::SetNoteText(const FText& NewText)
+{
+	NoteText = NewText;
+	NoteTextRenderer->SetText(NewText);
+}
+
+void AStickerNote::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
