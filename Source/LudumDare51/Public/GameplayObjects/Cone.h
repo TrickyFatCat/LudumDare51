5f8c77a868c098727f8ad87126b34f2bbcb549a9
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-02 15:36:36 +0300

    Create damaging cone

---
+++ Source/LudumDare51/Public/GameplayObjects/Cone.h
@@ -0,0 +1,47 @@
+﻿// "Copyright (c) 2022. Made by Title Goose Team during LudumDare 51. All rights reserved. "
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Cone.generated.h"
+
+class UBaseCapsuleTriggerComponent;
+
+UCLASS()
+class LUDUMDARE51_API ACone : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACone();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float StunDuration = 0.75f;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category=Components)
+	USceneComponent* ConeRoot = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Components)
+	UStaticMeshComponent* ConeMesh = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Components)
+	UBaseCapsuleTriggerComponent* DamageTrigger = nullptr;
+
+	UFUNCTION()
+	void OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+	                           AActor* OtherActor,
+	                           UPrimitiveComponent* OtherComp,
+	                           int32 OtherBodyIndex,
+	                           bool bFromSweep,
+	                           const FHitResult& SweepResult);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float LaunchPower = 1000.f;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
